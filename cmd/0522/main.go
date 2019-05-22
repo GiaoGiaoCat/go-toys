47f3e6361a55bca550203f027defd270fa69ae04
Victor Wang <wjp2013@gmail.com>
2019-05-22 21:56:22 +0800

    done

--- cmd/0522/main.go
+++ cmd/0522/main.go
@@ -16,24 +16,24 @@ import (
 const (
 	endPoint = "https://hist-quote.1tokentrade.cn"
 	format   = "2006-01-02"
-	otKey    = "XXX"
+	otKey    = "xxx"
 )
 
 var exchanges = []string{
 	"okex",
-	// "huobip",
+	"huobip",
 }
 
 var symbols = []string{
-	"btc.usdt",
+	// "btc.usdt",
 	// "eth.usdt",
 	// "xrp.usdt",
 	// "eos.usdt",
-	// "ltc.usdt",
-	// "etc.usdt",
-	// "bch.usdt",
-	// "trx.usdt",
-	// "bsv.usdt",
+	"ltc.usdt",
+	"etc.usdt",
+	"bch.usdt",
+	"trx.usdt",
+	"bsv.usdt",
 }
 
 type Candle struct {
@@ -52,11 +52,11 @@ func main() {
 			fmt.Println("开始处理交易对", symbol)
 
 			// 开始时间是 2018-04-01
-			// t := time.Date(2018, 3, 27, 00, 00, 01, 123456789, time.Now().Location())
-			t := time.Date(2019, 4, 27, 00, 00, 01, 123456789, time.Now().Location())
+			t := time.Date(2018, 3, 27, 00, 00, 01, 123456789, time.Now().Location())
+			// t := time.Date(2019, 4, 27, 00, 00, 01, 123456789, time.Now().Location())
 
 			// 缓存需要写入文件的字符串
-			// var tmpSlice []string
+			var tmpSlice []string
 
 			for {
 				// 计算下一个周五
@@ -64,16 +64,15 @@ func main() {
 
 				// 计算出来的周五超过今天，说明是未来的日子，直接中断
 				if t.After(time.Now()) {
-					// fmt.Println(tmpSlice)
-					// writeLines(tmpSlice, exchange+"-"+symbol+".csv")
+					writeLines(tmpSlice, exchange+"-"+symbol+".csv")
 					break
 				}
 
 				// fmt.Println(t.Format(format))
 				result := getCandleClose(exchange+"/"+symbol, t.Format(format), t.AddDate(0, 0, 1).Format(format))
-				// tmpSlice = append(tmpSlice, result)
-				fmt.Println(result)
-				time.Sleep(time.Millisecond * 500)
+				tmpSlice = append(tmpSlice, result)
+				// fmt.Println(result)
+				// time.Sleep(time.Millisecond * 100)
 			}
 		}
 	}
@@ -84,15 +83,10 @@ func getCandleClose(contract, since, until string) (result string) {
 	contents, _ := getCandles(contract, since, until)
 	json.Unmarshal(contents, &candles)
 
-	fmt.Println(contents)
-	fmt.Println(candles)
-
 	for _, candle := range candles {
 		hour := time.Unix(candle.Timestamp, 0).Hour()
 		min := time.Unix(candle.Timestamp, 0).Minute()
 		if hour == 16 && min == 2 {
-			fmt.Println(candle)
-			// fmt.Println(printCandle(candle))
 			result = printCandle(candle)
 			break
 		}
@@ -102,14 +96,12 @@ func getCandleClose(contract, since, until string) (result string) {
 
 func printCandle(c Candle) string {
 	date := time.Unix(c.Timestamp, 0).Format("2006-01-02T15:04:05Z07:00")
-	s := fmt.Sprintf("%s,%g", date, c.Close)
-	// return date + "," + strconv.FormatFloat(c.Close, 'f', 6, 64)
-	fmt.Println(s)
-	return s
+	return fmt.Sprintf("%s,%g", date, c.Close)
 }
 
 func getCandles(contract, since, until string) ([]byte, error) {
-	url := endPoint + packagePATH(contract, since, until)
+	path := "/candles?contract=" + contract + "&since=" + since + "&until=" + until + "&duration=1m&format=json"
+	url := endPoint + path
 
 	client := &http.Client{}
 	fmt.Println(url)
@@ -133,10 +125,6 @@ func getCandles(contract, since, until string) ([]byte, error) {
 	return body, nil
 }
 
-func packagePATH(contract, since, until string) string {
-	return "/candles?contract=" + contract + "&since=" + since + "&until=" + until + "&duration=1m&format=json"
-}
-
 // NOTE: 传入的日期在周 5，6，7 的时候，此函数工作正常
 func getNextFriday(date time.Time) time.Time {
 	return now.New(date).Monday().AddDate(0, 0, 11)
@@ -151,9 +139,7 @@ func writeLines(lines []string, outputFile string) error {
 	// new writer w/ default 4096 buffer size
 	w := bufio.NewWriter(file)
 
-	fmt.Println(lines)
 	for _, line := range lines {
-		fmt.Println(line)
 		_, err := w.WriteString(line + "\n")
 		check(err)
 	}
